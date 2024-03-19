063db73e04c8e69975ccb6e701c4f69e631f3b8c
thejusha <163998549+thejusha@users.noreply.github.com>
2024-03-19 16:36:57 +0700

    Add files via upload

---
+++ demo100.java
@@ -0,0 +1,29 @@
+class demo100
+{
+ public static void main(string a[])
+ {
+  int a,b,c;
+   a=10;
+   b=20;
+   c=30;
+  if(a>b)
+   {
+  if(a>c)
+    {
+     sysyem.out.print("a is greater");
+    }
+  else
+    {
+     system.out.print(" c is greater");
+    }
+  else
+  if(b>c)
+    }
+     system.out.print("b is greater");
+    }
+  else
+    {
+     system.out.print("c is greater");
+   }
+ }
+}  
